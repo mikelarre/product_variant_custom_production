50573b8e16ed4f43e7568833cf91ff03a94b87a8
mikelarre <mikelarregi@avanzosc.es>
2020-03-13 14:47:28 +0100

    [ADD + FIX] product_variant_custom_sale_mrp_link + mrp_production_custom_variants_inherited_attributes: added domain in scheduled product lines

---
+++ product_variant_custom_sale_mrp_link/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_mrp_link", "product_variant_custom",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "",
+    "data": [
+         ""
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': True,
+}
---
+++ product_variant_custom_sale_mrp_link/models/sale_order_line.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from . import api, fields, models, exceptions, _
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    def _action_create_mrp_dict(self):
+        if not self.product_id:
+            raise exceptions.Warning(_("select a product before create a "
+                                       "manufaturing order"))
+        if self.custom_value_lines and not self.product_version_id:
+            raise exceptions.Warning(_("select a version before create a "
+                                       "manufaturing order"))
+        res = super()._action_mrp_dict()
+        res['product_attribute_ids'] = \
+            self.product_id._get_product_attributes_values_dict()
+        res['custom_value_ids'] = self._set_custom_lines()
+        return res
