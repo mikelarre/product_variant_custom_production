a41efb6a6a0d3b17306d20957c1038d4b65260b7
odoo <mikelarregi@avanzosc.es>
2020-06-11 16:01:19 +0200

    [FIX] module name changed

--- product_variant_custom_sale_mrp_link/__manifest__.py
+++ product_variant_custom_sale_mrp_link/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright 2020 Mikel Arregi Etxaniz - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
-    "name": "Glue module sale_mrp_link version modules",
+    "name": "Glue module sale_line_mrp_link version modules",
     "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
-        "sale_mrp_link",
+        "sale_line_mrp_link",
         "product_variant_custom_sale",
         "mrp_production_custom_variants_inherited_attributes",
     ],
